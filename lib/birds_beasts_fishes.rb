7b39ef827a1d9a26f2f16cc9fc5679d3dfb85489
Hennie Bruurmijn <hlm.bruurmijn@gmail.com>
2022-05-27 02:08:21 +0200

    Add methods for updating outputs and check result

--- lib/birds_beasts_fishes.rb
+++ lib/birds_beasts_fishes.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
+require 'yaml'
+
 # Hangman game
 class Game
   def initialize
-    @word = sample_word_list
-    @board = Array.new(@word.length, 'X')
+    @word = sample_word_list.upcase.split('')
+    @board = Array.new(@word.length, '|_|')
     @available_letters = [*('A'..'Z')]
-    @turns = 0
+    @turns = 1
+    @hangman = 0
     play
   end
 
@@ -20,14 +23,74 @@ class Game
 
   def play
     display_state
+    evaluate(guess)
+    draw_hangman
+    @turns += 1
+    play
   end
 
   def display_state
-    @turns += 1
-    puts "Turn ##{@turns} of 10. The word consists of #{@word.length} letters:\n"
+    puts "Turn ##{@turns}. The word consists of #{@word.length} letters:\n"
     puts @board.join(' ')
     puts "\nThese are the remaining letters to chose from:\n#{@available_letters.join(' ')}"
-    puts 'Please enter your next guess:'
+    puts "Please enter your next guess (type 'exit' to save the game for later):"
+  end
+
+  def guess
+    guessed_letter = gets.chomp.upcase
+    if guessed_letter == 'EXIT'
+      save
+    elsif @available_letters.include?(guessed_letter)
+      guessed_letter
+    else
+      puts 'Please type a letter that is still available:'
+      guess
+    end
+  end
+
+  def draw_hangman
+    puts 'Draw hangman'
+    puts "#{7 - @hangman} lives left"
+  end
+
+  def evaluate(guess)
+    if word.include?(guess)
+      puts "'#{guess}' is in the word!"
+      word.each_index { |i| @board[i] = guess if word[i] == guess }
+    else
+      puts "'#{guess}' is not in the word"
+      @hangman += 1
+    end
+    @available_letters[@available_letters.index(guess)] = '-'
+    check_result
+  end
+
+  def check_result
+    word = @word.join
+    if @board == @word
+      puts "You've guessed the word: #{word}"
+      new_game
+    elsif @hangman == 7
+      puts "You've lost the game! The word was: #{word}"
+      new_game
+    end
+  end
+
+  def new_game
+    puts 'Do you want to play a new game? (y/n)'
+    new_game = gets.chomp.downcase
+    if new_game == 'y'
+      Game.new
+    else
+      abort('Thanks for playing! Go learn some more names of birds, beasts and fishes! See you back soon!')
+    end
+  end
+
+  def save
+    # dump = YAML.dump(self)
+    # puts dump
+    abort('game saved!')
+    # YAML.load(dump)
   end
 end
 
